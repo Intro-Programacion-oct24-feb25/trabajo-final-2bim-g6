ee82c9cde5d65d37debbf34f1820ab6b9fc1dc02
Josue00422 <jjnaula3@utpl.edu.ec>
2025-02-03 20:46:28 -0500

    Update TrabajoFinal.java

--- proyecto_java/TrabajoFinal/src/trabajofinal/TrabajoFinal.java
+++ proyecto_java/TrabajoFinal/src/trabajofinal/TrabajoFinal.java
@@ -13,93 +13,65 @@ import static funcion05.funcion05.funcion05;
 import static funcion06.funcion06.funcion06;
 import static funcion07.funcion07.funcion07;
 
-<<<<<<< HEAD
-/**
- *
- * @author USUARIO
- */
-public class TrabajoFinal {
-=======
 public class trabajoFinal {
->>>>>>> deec99f34b9da7bad67825cfe6102b5d213b66a6
-
+    public static String cadena = "Reporte\n";
+    
     public static void main(String[] args) {
+        
 
         Scanner entrada = new Scanner(System.in);
-        int contadorActividades = 0;
-        int[] contadorActividades1 = new int[7];
-        String[] deporte = {"Futbol",
-            "Natacion", "Atletismo", "Basquetbol", "Ciclismo", "Tenis", "Yoga"};
+        String[] areas = {"Futbol", "Natacion", "Atletismo", "Basquetbol", "Ciclismo", "Tenis", "Yoga"};
         int[] resultados = new int[7];
         int totalActividades = 0;
         boolean bandera = true;
 
         while (bandera) {
-<<<<<<< HEAD
-            System.out.println("Ingrese la diciplina  a la que quiere ingresar: ");
-            for (int i = 0; i < deporte.length; i++) {
-                System.out.println((i + 1) + "." + deporte[i] + "(" + (i + 1) + ")");
-=======
             System.out.println("Ingrese la disciplina a la que quiere ingresar: ");
             for (int i = 0; i < areas.length; i++) {
                 System.out.println((i + 1) + "." + areas[i]);
->>>>>>> deec99f34b9da7bad67825cfe6102b5d213b66a6
             }
             int opcion = entrada.nextInt();
             entrada.nextLine(); 
 
             switch (opcion) {
                 case 1:
-                    funcion01();
+                    cadena= funcion01(cadena);
                     resultados[0]++;
                     totalActividades++;
                     break;
                 case 2:
-                    funcion02();
+                    cadena =funcion02(cadena);
                     resultados[1]++;
                     totalActividades++;
                     break;
                 case 3:
-                    funcion03();
+                    cadena = funcion03(cadena);
                     resultados[2]++;
                     totalActividades++;
                     break;
                 case 4:
-                    funcion04();
+                    cadena = funcion04(cadena);
                     resultados[3]++;
                     totalActividades++;
                     break;
                 case 5:
-                    funcion05();
+                     cadena = funcion05(cadena);
                     resultados[4]++;
                     totalActividades++;
                     break;
                 case 6:
-                    funcion06();
+                    cadena = funcion06(cadena);
                     resultados[5]++;
                     totalActividades++;
                     break;
                 case 7:
-                    funcion07();
+                    cadena = funcion07(cadena);
                     resultados[6]++;
                     totalActividades++;
                     break;
                 default:
-<<<<<<< HEAD
-                    System.out.println("lo sentimos, el club no tiene esa opción.\n");
-            }
-
-            System.out.print("¿Desea salor del ciclo? Coloque(s) si desea salir del ciclo ");
-            String respuesta = entrada.nextLine();
-            if (respuesta.equalsIgnoreCase("s")) {
-                bandera = false;
-
-                System.out.println("Lo sentimos, el club no tiene esta opción");
-                bandera = false;
-=======
                     System.out.println("Lo sentimos, el club no tiene esta opción.");
                     bandera = false;
->>>>>>> deec99f34b9da7bad67825cfe6102b5d213b66a6
             }
 
             System.out.println("¿Desea continuar agregando actividades? Ingrese 'si' o 'no'");
@@ -111,6 +83,8 @@ public class trabajoFinal {
 
         obtenerInformacion(totalActividades);
         obtenerReporte(areas, resultados);
+                System.out.println(cadena);
+
     }
 
    
